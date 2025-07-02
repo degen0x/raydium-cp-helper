687d37b974c6ee2c1b2e78e943c427af1f5fc26b
degen0x <degen0x@gmail.com>
2025-07-03 00:51:16 +0300

    second commit

--- programs/cp-swap/src/lib.rs
+++ programs/cp-swap/src/lib.rs
@@ -1,12 +1,6 @@
-pub mod curve;
-pub mod error;
-pub mod instructions;
 pub mod states;
-pub mod utils;
 
-use crate::curve::fees::FEE_RATE_DENOMINATOR_VALUE;
 use anchor_lang::prelude::*;
-use instructions::*;
 
 #[cfg(feature = "devnet")]
 declare_id!("CPMDWBwJDtYax9qW7AyRuVC19Cc4L4Vcy4n2BHAbHkCW");
