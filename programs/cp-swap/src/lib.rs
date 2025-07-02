6af2ef0d708132710a8a7014819f11c65c1c9085
degen0x <degen0x@gmail.com>
2025-07-03 00:37:24 +0300

    first commit

---
+++ programs/cp-swap/src/lib.rs
@@ -0,0 +1,19 @@
+pub mod curve;
+pub mod error;
+pub mod instructions;
+pub mod states;
+pub mod utils;
+
+use crate::curve::fees::FEE_RATE_DENOMINATOR_VALUE;
+use anchor_lang::prelude::*;
+use instructions::*;
+
+#[cfg(feature = "devnet")]
+declare_id!("CPMDWBwJDtYax9qW7AyRuVC19Cc4L4Vcy4n2BHAbHkCW");
+#[cfg(not(feature = "devnet"))]
+declare_id!("CPMMoo8L3F4NbTegBCKVNunggL7H1ZpdTHKxQB5qKP1C");
+
+#[program]
+pub mod raydium_cp_swap {
+    use super::*;
+}
